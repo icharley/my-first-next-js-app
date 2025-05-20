39447a54d088e0fe886ea59554a398c1c40b4c32
icharley <eng.charlesegwuh@gmail.com>
2025-05-20 10:19:13 -0700

    order placed created.

---
+++ app/order-placed/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+
+const pages = () => {
+  return (
+    <div>
+      <IoCheckmarkCircleOutline />
+      <h1>Congratulations your order has been Placed</h1>
+    </div>
+  );
+};
+
+export default pages;
--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -11,16 +11,18 @@ const page = ({ params }) => {
   const product = products.find((p) => p.id === parseInt(params.id));
 
   return (
-    <div className="flex flex-row gap-6  items-center justify-center mx-15 mt-20">
+    <div className="flex lg:flex-row md:flex-col sm:flex-col gap-6  items-center justify-center mx-15 mt-20">
       <img src={product.image} alt="" />
       <div>
         <h1 className="font-bold text-4xl">{product.name}</h1>
         <p>${product.price}</p>
         <p className="max-w-150 ">{product.description}</p>,
-        <div className="flex flex-row items-center justify-between mt-4">
-          <FiMinus />
-          <p>1</p>
-          <FiPlus />
+        <div className="flex flex-row  items-center justify-between mt-4">
+          <div className="flex flex-row items-center gap-30">
+            <FiMinus />
+            <p>1</p>
+            <FiPlus />
+          </div>
           <button className="bg-black text-white px-8 py-4 flex flex-col">
             Add to Cart
           </button>
@@ -28,6 +30,11 @@ const page = ({ params }) => {
         <p className="text-left mt-5 flex flex-row items-center gap-2">
           <CiHeart /> Add to wishlist
         </p>
+        <div className="flex flex-row gap-4 mt-5 items-center text-4xl">
+          <FaInstagram />
+          <FaFacebookSquare />
+          <FaTwitterSquare />
+        </div>
       </div>
     </div>
   );
