e9532214b092cf66ef9d65523558aaf9837a185e
icharley <eng.charlesegwuh@gmail.com>
2025-05-20 09:50:38 -0700

    Designed my first dynamic routing for each product

---
+++ app/product/[id]/page.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { products } from "@/product";
+import { FiMinus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
+import { CiHeart } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+
+const page = ({ params }) => {
+  const product = products.find((p) => p.id === parseInt(params.id));
+
+  return (
+    <div className="flex flex-row gap-6  items-center justify-center mx-15 mt-20">
+      <img src={product.image} alt="" />
+      <div>
+        <h1 className="font-bold text-4xl">{product.name}</h1>
+        <p>${product.price}</p>
+        <p className="max-w-150 ">{product.description}</p>,
+        <div className="flex flex-row items-center justify-between mt-4">
+          <FiMinus />
+          <p>1</p>
+          <FiPlus />
+          <button className="bg-black text-white px-8 py-4 flex flex-col">
+            Add to Cart
+          </button>
+        </div>
+        <p className="text-left mt-5 flex flex-row items-center gap-2">
+          <CiHeart /> Add to wishlist
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- product.js
+++ product.js
@@ -6,6 +6,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-9.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 2,
@@ -14,6 +16,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/main_big-5.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-10.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 4,
@@ -30,6 +36,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-12.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 5,
@@ -38,6 +46,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-11.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 6,
@@ -46,6 +56,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-6.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 7,
@@ -54,6 +66,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-7.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 8,
@@ -62,6 +76,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-5.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 9,
@@ -70,6 +86,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-8.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 10,
@@ -78,6 +96,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-13.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 11,
@@ -86,6 +106,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 12,
@@ -94,6 +116,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-4.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 13,
@@ -102,6 +126,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-2.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 14,
@@ -110,6 +136,8 @@ export const products = [
     price: 10.0,
     image:
       "https://masonryshop.liquid-themes.com/wp-content/uploads/2021/11/Mask-3.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
 
